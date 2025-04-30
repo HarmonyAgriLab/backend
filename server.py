6064ab5b30c6cf49e1424805e3c357a5fb5a9f8c
十一 <13515530+zhang-yukai-eleven@user.noreply.gitee.com>
2025-04-30 12:04:39 +0800

    update publish.py and server.py

--- server.py
+++ server.py
@@ -9,6 +9,8 @@ from fastapi.responses import FileResponse
 
 from get_config import *
 
+from publish import publish_ceiling, publish_fan, publish_curtain, publish_heater
+
 mysql_config_list = get_mysql_config()
 username = mysql_config_list['username']
 password = mysql_config_list['password']
@@ -100,10 +102,103 @@ def get_soil_data(db: Session = Depends(get_db)):
     soil_data = db.query(Soil).order_by(Soil.id.desc()).limit(20).all()
     return soil_data
 
+# 获取图片
 @app.get("/images/{image_name}")
 async def get_image(image_name: str):
     image_path = f'/root/images/{image_name}'
     return FileResponse(image_path, media_type = 'image/png')
+
+# 控制棚顶开合
+@app.get("/controll/ceiling/{status}")
+def controll_ceiling(status: str):
+    try:
+        status_int = int(status)
+        publish_ceiling(status_int)
+        result = {
+            "code": 0,
+            "message": "Success." 
+        }
+        return result
+    except ValueError:
+        result = {
+            "code": 1,
+            "message": "Fail, invalid type of status."
+        }
+        return result
+    except Exception as e:
+        result = {
+            "code": 1,
+            "message": "Fail, unexpected error occurred."
+        }
+
+# 控制排风扇开关
+@app.get("/controll/fan/{status}")
+def controll_fan(status: str):
+    try:
+        status_int = int(status)
+        publish_fan(status_int)
+        result = {
+            "code": 0,
+            "message": "Success."
+        }
+        return result
+    except ValueError:
+        result = {
+            "code": 1,
+            "message": "Fail, invalid type of status."
+        }
+        return result
+    except Exception as e:
+        result = {
+            "code": 1,
+            "message": "Fail, unexpected error occurred."
+        }
+
+# 控制遮阳帘开闭
+@app.get("/controll/curtain/{status}")
+def controll_curtain(status: str):
+    try:
+        status_int = int(status)
+        publish_curtain(status_int)
+        result = {
+            "code": 0,
+            "message": "Success."
+        }
+        return result
+    except ValueError:
+        result = {
+            "code": 1,
+            "message": "Fail, invalid type of status."
+        }
+        return result
+    except Exception as e:
+        result = {
+            "code": 1,
+            "message": "Fail, unexpected error occurred."
+        }
+
+# 控制地暖开关
+@app.get("/controll/heater/{status}")
+def controll_heater(status: str):
+    try:
+        status_int = int(status)
+        publish_heater(status_int)
+        result = {
+            "code": 0,
+            "message": "Success."
+        }
+        return result
+    except ValueError:
+        result = {
+            "code": 1,
+            "message": "Fail, invalid type of status."
+        }
+        return result
+    except Exception as e:
+        result = {
+            "code": 1,
+            "message": "Fail, unexpected error occurred."
+        }
 
 # 启动应用
 if __name__ == "__main__":
